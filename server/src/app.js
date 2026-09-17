c1a4de9da19d760a73d33954b18ce09555e9332f
Adarsh Jayasanker <adarshcodes1@gmail.com>
2026-09-18 00:15:51 +0530

    ds

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ app.set("trust proxy", 1);
 
 app.use(requestId);
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
 }));
 app.use(express.json());
